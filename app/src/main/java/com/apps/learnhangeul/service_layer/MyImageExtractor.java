2707cf76e17fe0718006e5d5d9dae34b069a84b9
RobertJif <roberttjjif@gmail.com>
2020-05-06 18:05:15 +0700

    fixing some logic

--- app/src/main/java/com/apps/learnhangeul/service_layer/MyImageExtractor.java
+++ app/src/main/java/com/apps/learnhangeul/service_layer/MyImageExtractor.java
@@ -20,7 +20,7 @@ public class MyImageExtractor {
     //Configurations adjust this to your liking
     private final int erodeAmount = 1;
     private final int binaryTreshold = 127;
-    private final int imageSize = 50;
+    private final int imageSize = 400;
 
     //Do not change this
     private final Mat kernel = Imgproc.getStructuringElement(Imgproc.CV_SHAPE_RECT,
@@ -35,26 +35,39 @@ public class MyImageExtractor {
         double smallestDistance = 0;
         int smallestDistanceIndex = 0;
 
+        double[] result = new double[data.length];
+        System.out.println("Mulai Pengenalan");
+
         for (int i = 0; i < data.length ; i++){
+            System.out.println("Data ke "+ i);
             Mat trained = Preprocess(data[i].getImage());
-
-            double[] result = new double[test.width()*test.height()];
-
+            int distance = 0;
             //Find the smallest distance
             for (int x = 0; x < test.width(); x++){
                 for (int y = 0; y < test.height(); y++){
                 result[i] = 0;
-                result[i] += Math.pow((test.get(y, x)[0] - trained.get(y, x)[0]), 2);
+                double testPixel = test.get(y, x)[0] / 255;
+                double trainedPixel = trained.get(y, x)[0] / 255;
+                double minus = (testPixel - trainedPixel);
+
+                distance += Math.pow(minus, 2);
+                //if (minus > 0){
+                    System.out.println(testPixel + " - " + trainedPixel +" pangkat 2 = " + Math.pow(minus, 2));
+                //}
+
                 }
             }
 
+            result[i] = Math.sqrt(distance);
             //Compare to get the smallest distance
-            if (smallestDistance > Math.sqrt(result[i])){
-                smallestDistance = Math.sqrt(result[i]);
-                smallestDistanceIndex = i;
-            }
-        }
+            //if (smallestDistance > Math.sqrt(result[i])){
+            //    smallestDistance = Math.sqrt(result[i]);
+            //   smallestDistanceIndex = i;
+            //}
 
+
+        }
+        smallestDistanceIndex = FindSmallestNumber(result);
         return data[smallestDistanceIndex];
     }
 
@@ -75,8 +88,23 @@ public class MyImageExtractor {
         threshold(result, result, binaryTreshold, 255, THRESH_BINARY);
 
         //erode image
-        erode(result, result, kernel);
+        //erode(result, result, kernel);
 
         return result;
+    }
+    private int FindSmallestNumber(double a[]){
+        double minimum;
+        int index=0,i=1;
+        minimum=a[0];
+        while(i<a.length)
+        {
+            if(minimum>a[i])
+            {
+                minimum=a[i];
+                index=i;
+            }
+            i++;
+        }
+        return index;
     }
 }
